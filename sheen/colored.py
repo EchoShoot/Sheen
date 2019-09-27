b9b31bcda60e3f6d18252c29483ef58f05c01ebd
Biar Fordlander <852822425@qq.com>
2019-09-27 12:42:56 +0800

    Update colored.py
    now, your can use ColoredHanlder  just like the StreamHandler, supported ``` import logging
    from sheen import ColoredHandler
    logging.StreamHandler = ColoredHandler
    logging.warning('yes')```

--- sheen/colored.py
+++ sheen/colored.py
@@ -8,7 +8,8 @@
 
 import logging
 from sheen.color import Str
-from logging import StreamHandler, _addHandlerRef, _checkLevel, NOTSET, Formatter
+import sys
+from logging import StreamHandler, _addHandlerRef, _checkLevel, NOTSET, Formatter, Handler, getLevelName
 
 # 公开给外部的函数 (interface)
 __all__ = (
@@ -31,7 +32,7 @@ FMT_PREFIX = Str(' ').join([FMT_TIME, FMT_NAME, FMT_LEVEL])
 # init Config
 FMT_DEFAULT = FMT_PREFIX + FMT_MSG
 FMT_DEBUG = FMT_PREFIX + Str.lightblack(FMT_MSG)
-FMT_INFO = FMT_PREFIX + Str.green(FMT_MSG)
+FMT_INFO = FMT_PREFIX + Str(FMT_MSG)
 FMT_WARNING = FMT_PREFIX + Str.yellow(FMT_MSG)
 FMT_ERROR = FMT_PREFIX + Str.red(FMT_MSG)
 FMT_CRITICAL = FMT_PREFIX + Str.RED(FMT_MSG)
@@ -39,15 +40,25 @@ FMT_CRITICAL = FMT_PREFIX + Str.RED(FMT_MSG)
 _defaultFormatter = Formatter(fmt=str(FMT_DEFAULT), datefmt=FMT_DATE)  # default Formatter
 
 
-class ColoredHandler(StreamHandler):
-    def __init__(self, level=NOTSET, stream=None):
+class ColoredHandler(Handler):
+    """
+    A handler class which writes logging records, appropriately formatted,
+    to a stream. Note that this class does not close the stream, as
+    sys.stdout or sys.stderr may be used.
+    """
+
+    terminator = '\n'
+
+    def __init__(self, stream=None):
         """
-        Initializes the instance - basically setting the formatter to None
-        and the filter list to empty.
+        Initialize the handler.
+
+        If stream is not specified, sys.stderr is used.
         """
-        StreamHandler.__init__(self, stream)
-        self._name = None
-        self.level = _checkLevel(level)
+        Handler.__init__(self)
+        if stream is None:
+            stream = sys.stderr
+        self.stream = stream
         self.formatter = {
             logging.DEBUG: Formatter(fmt=str(FMT_DEBUG), datefmt=FMT_DATE),
             logging.INFO: Formatter(fmt=str(FMT_INFO), datefmt=FMT_DATE),
@@ -55,16 +66,25 @@ class ColoredHandler(StreamHandler):
             logging.ERROR: Formatter(fmt=str(FMT_ERROR), datefmt=FMT_DATE),
             logging.CRITICAL: Formatter(fmt=str(FMT_CRITICAL), datefmt=FMT_DATE),
         }
-        # Add the handler to the global _handlerList (for cleanup on shutdown)
-        _addHandlerRef(self)
-        self.createLock()
 
     def format(self, record):
         fmt = self.formatter.get(record.levelno, None)
         if fmt is None:
             fmt = _defaultFormatter
         return fmt.format(record)
-        
+
+    def setFormatter(self, fmt):
+        """
+        Set the formatter for this handler.
+        """
+        self.formatter = {
+            logging.DEBUG: Formatter(fmt=str(Str.lightblack(fmt._fmt)), datefmt=fmt.datefmt),
+            logging.INFO: Formatter(fmt=str(Str(fmt._fmt)), datefmt=fmt.datefmt),
+            logging.WARNING: Formatter(fmt=str(Str.yellow(fmt._fmt)), datefmt=fmt.datefmt),
+            logging.ERROR: Formatter(fmt=str(Str.red(fmt._fmt)), datefmt=fmt.datefmt),
+            logging.CRITICAL: Formatter(fmt=str(Str.RED(fmt._fmt)), datefmt=fmt.datefmt),
+        }
+
     def setFormatters(self, fmts):
         """
             Design colored logger for yourself to integrate more imagination.
@@ -90,19 +110,78 @@ class ColoredHandler(StreamHandler):
             [!] the parameter fmts receive format like this (support 2 types):
             ```
                 handle = ColoredHandle()
-                handle.setFormatter({
+                handle.setFormatters({
                     logging.DEBUG: Formatter(fmt=str(Str.lightblack(fmt)), datefmt=datefmt), # 'Formatter' Type
                     logging.INFO: Str.green(fmt),  # 'Str' or 'str' Type
                 })
             ```
             """)
 
+    def flush(self):
+        """
+        Flushes the stream.
+        """
+        self.acquire()
+        try:
+            if self.stream and hasattr(self.stream, "flush"):
+                self.stream.flush()
+        finally:
+            self.release()
+
+    def emit(self, record):
+        """
+        Emit a record.
+
+        If a formatter is specified, it is used to format the record.
+        The record is then written to the stream with a trailing newline.  If
+        exception information is present, it is formatted using
+        traceback.print_exception and appended to the stream.  If the stream
+        has an 'encoding' attribute, it is used to determine how to do the
+        output to the stream.
+        """
+        try:
+            msg = self.format(record)
+            stream = self.stream
+            # issue 35046: merged two stream.writes into one.
+            stream.write(msg + self.terminator)
+            self.flush()
+        except Exception:
+            self.handleError(record)
+
+    def setStream(self, stream):
+        """
+        Sets the StreamHandler's stream to the specified value,
+        if it is different.
+
+        Returns the old stream, if the stream was changed, or None
+        if it wasn't.
+        """
+        if stream is self.stream:
+            result = None
+        else:
+            result = self.stream
+            self.acquire()
+            try:
+                self.flush()
+                self.stream = stream
+            finally:
+                self.release()
+        return result
+
+    def __repr__(self):
+        level = getLevelName(self.level)
+        name = getattr(self.stream, 'name', '')
+        if name:
+            name += ' '
+        return '<%s %s(%s)>' % (self.__class__.__name__, name, level)
+
 
 if __name__ == '__main__':
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
 
-    handle = ColoredHandle()
+    handle = ColoredHandler()
+#    handle.setFormatter(Formatter(fmt='%(asctime)s - %(levelname)s | %(message)s'))
     handle.setFormatters({
         logging.DEBUG: Formatter(fmt=str(Str.blue('%(asctime)s - %(levelname)s | %(message)s')), datefmt=FMT_DATE),
         logging.INFO: Str.magenta('%(asctime)s - %(levelname)s | %(message)s'),
